0c459ff012ec0ef816e8960e79c26909338c53e0
Armelle Loiseau <armelle.loiseau@gmail.com>
2022-01-03 18:07:54 +0100

    Main layout

---
+++ public/js/canvas.js
@@ -0,0 +1,24 @@
+// home page circle
+const canvas = document.querySelector("#demo-reel");
+console.log(canvas);
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(150, 150, 148, 0, Math.PI * 2);
+ctx.lineWidth = 2;
+ctx.strokeStyle = "black";
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.setLineDash([5, 5]);
+ctx.moveTo(150, 2);
+ctx.lineTo(150, 300);
+ctx.stroke();
+ctx.closePath();
+
+ctx.font = "30px 'Roboto'";
+ctx.fillText("Web", 50, 150);
+
+ctx.font = "30px 'Roboto'";
+ctx.fillText("Docs", 200, 150);
